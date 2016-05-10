c39cefec02269a86b125ffcb359c2fe2cb70a2c9
Francesco <evolved37@gmail.com>
2016-05-10 22:56:02 +0200

    Update views_search_index.php

--- protected/humhub/modules/search/messages/it/views_search_index.php
+++ protected/humhub/modules/search/messages/it/views_search_index.php
@@ -1,8 +1,12 @@
 <?php
 return array (
+  'Advanced search settings' => 'Impostazioni di ricerca avanzata',
+  'Search for user, spaces and content' => 'Cerca per utente, spazi e contenuti',
   '<strong>Search </strong> results' => 'Risultati di <strong>Ricerca</strong>',
   'All' => 'Tutti',
   'Content' => 'Contenuti',
   'Spaces' => 'Space',
-  'Users' => 'Persone',
+  'Users' => 'Utenti',
+  'Search only in certain spaces:'=>'Cerca solo in alcuni spazi:',
+  'Specify space' => 'Aggiungi uno spazio'
 );
