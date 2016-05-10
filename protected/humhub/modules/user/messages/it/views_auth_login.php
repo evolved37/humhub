57ac179d8e00199f83389c8099b72d6846ca6ad8
Francesco <evolved37@gmail.com>
2016-05-10 22:26:18 +0200

    Update views_auth_login.php

--- protected/humhub/modules/user/messages/it/views_auth_login.php
+++ protected/humhub/modules/user/messages/it/views_auth_login.php
@@ -3,6 +3,7 @@ return array (
   '<strong>Please</strong> sign in' => '<strong>Per favore</strong> accedi',
   '<strong>Sign</strong> up' => '<strong>Registrati</strong>',
   'Create a new one.' => 'Creane una nuova.',
+  'remember Me' => 'Accesso automatico',
   'Don\'t have an account? Join the network by entering your e-mail address.' => 'Non hai un account? Aggiungiti al network inserendo il tuo indirizzo e-mail.',
   'Forgot your password?' => 'Hai dimenticato la password?',
   'If you\'re already a member, please login with your username/email and password.' => 'Se sei già un membro, per favore accedi con il tuo username/email e password.',
